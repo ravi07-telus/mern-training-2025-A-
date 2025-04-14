cb0b715955806cb9b919f0390c2c670f4c2530ed
ravi kumar <ravi.kumar07@telusinternational.com>
2025-04-14 15:26:52 +0530

    initial commit

---
+++ Javascript/day-1/app.js
@@ -0,0 +1,9 @@
+/*
+    1) Introduction to JavaScript: history, where it runs:
+    2) Variables Declaration (let, const, var)
+    3) Data types (String, Number, Boolean, Null, Undefined, Array, Object)
+    4) Operators (Arithmetic, Comparison, Logical)
+    5) if, else, switch
+    6) Loops: for, while, do...while, for...of, for...in
+    7) break and continue
+*/
