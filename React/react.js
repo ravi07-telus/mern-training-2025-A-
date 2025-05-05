a2a29385ad8704a5bc2b6fad090a5b2cab5f9bad
ravi kumar <ravi.kumar07@telusinternational.com>
2025-05-05 12:29:28 +0530

    added cariculam of react

---
+++ React/react.js
@@ -0,0 +1,22 @@
+/*
+==> Things we would be learning:
+    1) What is React and why do we use them ?
+    2) Installation of react app using (create-react-app)
+    3) Create a todo-app using create react app:
+    4) Features of Todo App: 
+        -> Add Task
+        -> Delete Task
+        -> List Tasks
+        -> Update Task
+        -> Mark tasks as completed
+        -> View completed tasks
+        -> Task Stats page (show incomplete and completed tasks count)
+        While developing these features explain below concepts as well:
+            ->> Components
+            ->> props
+            ->> States
+            ->> Life-Cycle-Methods
+            ->> Actual Vs Virtual DOM
+            ->> How React works
+    5) Create Home, About and Dashboard pages to demonstrate React Routers concepts
+*/
